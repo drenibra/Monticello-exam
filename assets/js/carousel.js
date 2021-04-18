b81303243e00487fff8102a7848228f17bdcdf8f
drenibra <drenibrahimi02@gmail.com>
2021-04-18 11:26:33 +0200

    hero carousel done + added extra images

---
+++ assets/js/carousel.js
@@ -0,0 +1,13 @@
+$('#owl-one').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:false,
+    autoplayTimeout:4000,
+    nav:true,
+    lazyLoad: true,
+    responsive:{
+      0:{
+        items:1
+      }
+    }
+});

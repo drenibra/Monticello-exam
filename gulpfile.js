f32ba214ae6419d7b14076684173990ded22de01
drenibra <drenibrahimi02@gmail.com>
2021-05-05 23:13:05 +0200

    used gulp to minify js and compress images

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const sourcemaps = require('gulp-sourcemaps');
+
+const { src, series, parallel, dest, watch } = require('gulp');
+
+const jsPath = 'assets/js/carousel.js';
+
+function imgTask() {
+    return src('assets/images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'));
+}
+
+function jsTask() {
+    return src(jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat('carousel.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist/assets/js'));
+}
+
+exports.jsTask = jsTask;
+exports.imgTask = imgTask;
+exports.default = parallel(imgTask, jsTask);
